e001d5f10aa50b4386b0c16eb026f006d4972ec4
CpyRubens <joaorubens2500@hotmail.com>
2022-07-28 15:11:42 -0300

    PaletaLista useEffect, criação do Header Home e css

--- cardapio-el-geladon/src/components/PaletaLista.jsx
+++ cardapio-el-geladon/src/components/PaletaLista.jsx
@@ -1,27 +1,40 @@
-import './PaletaLista.css'
-import {paletas} from '../mocks/paletas.js'
+import React, { useState } from "react";
+import "./PaletaLista.css";
+import { paletas } from "../mocks/paletas.js";
 
 function PaletaLista() {
+  const [paletaSelecionada, setPaletaSelecionada] = useState({});
+
+  const adicionarItem= (paletaIndex) =>{
+    const paleta = {
+      [paletaIndex]:Number(paletaSelecionada[paletaIndex]|| 0) +1
+    }
+    setPaletaSelecionada({...paletaSelecionada,...paleta})
+  }
+
   return (
     <div className="PaletaLista">
-      {paletas.map((paletas, index)=> (
-      <div className="PaletaListaItem" key={`PaletaListaItem ${index}`}>
-        <div>
-          <div className="PaletaListaItem__titulo">{paletas.titulo}</div>
-          <div className="PaletaListaItem__preco">{paletas.preco}</div>
-          <div className="PaletaListaItem__descricao">{paletas.descricao}</div>
-          <div className="PaletaListaItem__acoes Acoes">
-            <button className="Acoes__adicionar Acoes__adicionar--preencher">
-              adicionar
-            </button>
+      {paletas.map((paletas, index) => (
+        <div className="PaletaListaItem" key={`PaletaListaItem ${index}`}>
+          <span className="PaletaListaItem__badge">{paletaSelecionada[index]||0}</span>
+          <div>
+            <div className="PaletaListaItem__titulo">{paletas.titulo}</div>
+            <div className="PaletaListaItem__preco">{paletas.preco}</div>
+            <div className="PaletaListaItem__descricao">
+              {paletas.descricao}
+            </div>
+            <div className="PaletaListaItem__acoes Acoes">
+              <button className="Acoes__adicionar Acoes__adicionar--preencher" onClick={()=> adicionarItem(index)}>
+                adicionar
+              </button>
+            </div>
           </div>
+          <img
+            className="PaletaListaItem__foto"
+            src={paletas.foto}
+            alt={paletas.sabor}
+          />
         </div>
-        <img
-          className="PaletaListaItem__foto"
-          src={paletas.foto}
-          alt={paletas.sabor}
-        />
-      </div>
       ))}
     </div>
   );
