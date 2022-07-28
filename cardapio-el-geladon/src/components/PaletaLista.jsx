deee2c3fd4e3b2fd17aa17923ffa3daf25e11654
CpyRubens <joaorubens2500@hotmail.com>
2022-07-28 15:55:07 -0300

    Update PaletaLista.jsx

--- cardapio-el-geladon/src/components/PaletaLista.jsx
+++ cardapio-el-geladon/src/components/PaletaLista.jsx
@@ -5,18 +5,26 @@ import { paletas } from "../mocks/paletas.js";
 function PaletaLista() {
   const [paletaSelecionada, setPaletaSelecionada] = useState({});
 
-  const adicionarItem= (paletaIndex) =>{
+  const adicionarItem = (paletaIndex) => {
     const paleta = {
-      [paletaIndex]:Number(paletaSelecionada[paletaIndex]|| 0) +1
-    }
-    setPaletaSelecionada({...paletaSelecionada,...paleta})
-  }
+      [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) + 1,
+    };
+    setPaletaSelecionada({ ...paletaSelecionada, ...paleta });
+  };
+
+  const badgeCounter = (canRender, index) =>
+    Boolean(canRender) && (
+      <span className="PaletaListaItem__badge">
+        {paletaSelecionada[index]}
+      </span>
+    );
 
   return (
     <div className="PaletaLista">
       {paletas.map((paletas, index) => (
         <div className="PaletaListaItem" key={`PaletaListaItem ${index}`}>
-          <span className="PaletaListaItem__badge">{paletaSelecionada[index]||0}</span>
+          {badgeCounter(paletaSelecionada[index], index)}
+
           <div>
             <div className="PaletaListaItem__titulo">{paletas.titulo}</div>
             <div className="PaletaListaItem__preco">{paletas.preco}</div>
@@ -24,7 +32,10 @@ function PaletaLista() {
               {paletas.descricao}
             </div>
             <div className="PaletaListaItem__acoes Acoes">
-              <button className="Acoes__adicionar Acoes__adicionar--preencher" onClick={()=> adicionarItem(index)}>
+              <button
+                className="Acoes__adicionar Acoes__adicionar--preencher"
+                onClick={() => adicionarItem(index)}
+              >
                 adicionar
               </button>
             </div>
