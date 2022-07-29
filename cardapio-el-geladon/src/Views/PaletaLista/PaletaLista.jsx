eeed6f0646c3420bfd1b1d9815d1656e7fdbd578
CpyRubens <joaorubens2500@hotmail.com>
2022-07-29 14:52:25 -0300

    Estrutura organizada de pastas

--- cardapio-el-geladon/src/Views/PaletaLista/PaletaLista.jsx
+++ cardapio-el-geladon/src/Views/PaletaLista/PaletaLista.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./PaletaLista.css";
-import { paletas } from "../mocks/paletas.js";
+import { paletas } from "mocks/paletas.js";
 
 function PaletaLista() {
   const [paletaSelecionada, setPaletaSelecionada] = useState({});
@@ -11,6 +11,12 @@ function PaletaLista() {
     };
     setPaletaSelecionada({ ...paletaSelecionada, ...paleta });
   };
+  const removerItem = (paletaIndex) => {
+    const paleta = {
+      [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) -1,
+    };
+    setPaletaSelecionada({ ...paletaSelecionada, ...paleta });
+  };
 
   const badgeCounter = (canRender, index) =>
     Boolean(canRender) && (
@@ -18,6 +24,17 @@ function PaletaLista() {
         {paletaSelecionada[index]}
       </span>
     );
+
+    const removeButton = (canRender, index) =>
+    Boolean(canRender) && (
+      <button
+    className="Acoes__remover"
+    onClick={() => removerItem(index)}
+  >
+    remover
+  </button>
+    );
+    
 
   return (
     <div className="PaletaLista">
@@ -33,11 +50,13 @@ function PaletaLista() {
             </div>
             <div className="PaletaListaItem__acoes Acoes">
               <button
-                className="Acoes__adicionar Acoes__adicionar--preencher"
+                className={`Acoes__adicionar ${!paletaSelecionada[index] && "Acoes__adicionar--preencher"}`}
                 onClick={() => adicionarItem(index)}
               >
                 adicionar
               </button>
+              {removeButton(paletaSelecionada[index],index)}
+             
             </div>
           </div>
           <img
