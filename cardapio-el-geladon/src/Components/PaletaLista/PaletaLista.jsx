ea7e2d59d1a50de27bd3264176918e7deb7e3d4a
CpyRubens <joaorubens2500@hotmail.com>
2022-08-04 17:01:32 -0300

    move images to public/assets

--- cardapio-el-geladon/src/Components/PaletaLista/PaletaLista.jsx
+++ cardapio-el-geladon/src/Components/PaletaLista/PaletaLista.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./PaletaLista.css";
-import { paletas } from "mocks/paletas.js";
+import { PaletaService } from "services/paletaService";
 import PaletaListaItem from "Components/PaletaListaItem/PaletaListaItem";
 function PaletaLista() {
+  const [paletas, setPaletas] = useState([]);
+
   const [paletaSelecionada, setPaletaSelecionada] = useState({});
 
   const adicionarItem = (paletaIndex) => {
@@ -16,19 +18,28 @@ function PaletaLista() {
       [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) - 1,
     };
     setPaletaSelecionada({ ...paletaSelecionada, ...paleta });
+
   };
 
+  const getLista = async () => {
+    const response = await PaletaService.getLista();
+    setPaletas(response);
+  };
+  
+  useEffect(() => {
+    getLista();
+  }, []);
 
   return (
     <div className="PaletaLista">
       {paletas.map((paletas, index) => (
-        <PaletaListaItem 
-        key={`PaletaListaItem-${index}`}
-        paleta={paletas}
-        quantidadeSelecionada={paletaSelecionada[index]}
-        index={index}
-        onAdd={index =>adicionarItem(index)}
-        onRemove ={index => removerItem(index)}
+        <PaletaListaItem
+          key={`PaletaListaItem-${index}`}
+          paleta={paletas}
+          quantidadeSelecionada={paletaSelecionada[index]}
+          index={index}
+          onAdd={(index) => adicionarItem(index)}
+          onRemove={(index) => removerItem(index)}
         />
       ))}
     </div>
