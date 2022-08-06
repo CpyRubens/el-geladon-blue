0bd0aab97b7de48c5b2898b91464dd8ec913c192
CpyRubens <joaorubens2500@hotmail.com>
2022-08-05 23:09:37 -0300

    updt

--- cardapio-el-geladon/src/Components/Navbar/Navbar.jsx
+++ cardapio-el-geladon/src/Components/Navbar/Navbar.jsx
@@ -1,31 +1,36 @@
-import './Navbar.css'
-import sacola from 'assets/icons/sacola.svg';
-import logo from 'assets/logo.svg';
+import "./Navbar.css";
+import paleta from "assets/icons/paleta.svg";
+import sacola from "assets/icons/sacola.svg";
+import logo from "assets/logo.svg";
 
-function Navbar(){
- return( <div className="Home__header Header">
- <div className="row">
-     <div className="Header__logo Logo">
-     <img
-         src={logo}
-         width="70px"
-         alt="Logo El Geladon"
-         className="Logo__icone"
-     />
-     <span className="Logo__titulo"> El Geladon </span>
-     </div>
-     <div className="Header__opcoes Opcoes">
-     <div className="Opcoes__sacola Sacola">
-         <img
-         src={sacola}
-         width="40px"
-         className="Sacola__icone"
-         alt="Sacola de compras"
-         />
-     </div>
-     </div>
- </div>
-</div>);   
+function Navbar({ createPaleta }) {
+  return (
+    <div className="Home__header Header">
+      <div className="row">
+        <div className="Header__logo Logo">
+          <img
+            src={logo}
+            width="70px"
+            alt="Logo El Geladon"
+            className="Logo__icone"
+          />
+          <span className="Logo__titulo"> El Geladon </span>
+        </div>
+        <div className="Header__opcoes Opcoes">
+          <button type="button" className="Opcoes__paleta Paleta" onClick={()=> createPaleta()}></button>
+          <img src={paleta} width="40px" className="Paleta__icone" alt="Adicionar Paleta" />
+          <div className="Opcoes__sacola Sacola">
+            <img
+              src={sacola}
+              width="40px"
+              className="Sacola__icone"
+              alt="Sacola de compras"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
--- cardapio-el-geladon/src/Views/Home/Home.jsx
+++ cardapio-el-geladon/src/Views/Home/Home.jsx
@@ -1,15 +1,24 @@
-import './Home.css'
-import PaletaLista  from 'Components/PaletaLista/PaletaLista';
-import Navbar from 'Components/Navbar/Navbar';
+import "./Home.css";
+import { useState } from "react";
+import PaletaLista from "Components/PaletaLista/PaletaLista";
+import Navbar from "Components/Navbar/Navbar";
+import AdicionaPaletaModal from "Components/AdicionaPaletaModal/AdicionaPaletaModal";
 function Home() {
-    return (
-      <div className="Home">
-       <Navbar />
+  const [canShowAdicionaPaletaModal, setCanShowAdicionaPaletaModal] =
+    useState(false);
+  return (
+    <div className="Home">
+      <Navbar createPaleta={() => setCanShowAdicionaPaletaModal(true)} />
       <div className="Home__container">
         <PaletaLista />
+        {canShowAdicionaPaletaModal && (
+          <AdicionaPaletaModal
+            closeModal={() => setCanShowAdicionaPaletaModal(false)}
+          />
+        )}
       </div>
     </div>
-    )
-  }
-  
-  export default Home;
+  );
+}
+
+export default Home;
